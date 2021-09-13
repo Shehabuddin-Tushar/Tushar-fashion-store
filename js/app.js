51779df052951fec402c703d85762bd8f7da4452
Tushar <freelancerrtushar@gmail.com>
2021-09-13 19:25:11 +0600

    link changes

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const loadProducts = () => {
   //const url = `https://fakestoreapi.com/products`;
-    //const url = `https://tushar-fashion-store.netlify.app/products.json`;
-    const url = `http://127.0.0.1:5500/products.json`;
+    const url = `https://tushar-fashion-store.netlify.app/products.json`;
+    
     fetch(url)
     .then((response) => response.json())
     .then((data) => showProducts(data));
